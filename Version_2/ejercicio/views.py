f34935e74fb8742ae08dec3bfd1c5828398c0fc2
unknown <1902140@utrivieramaya.edu.mx>
2021-06-28 23:34:33 -0500

    Version_2

---
+++ Version_2/ejercicio/views.py
@@ -0,0 +1,10 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render (request, 'ejercicio/home.html', {'nombre':'darwin gonzalez'})
+
+def otra_pagina(request):
+    return HttpResponse ('<h1>Esta es otra pagina</h1>')
